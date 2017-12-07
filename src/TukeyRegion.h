8a0d4d849ae3ba11e3b359102434175eacaef7e3
Pavlo Mozharovskyi <pavlo.mozharovskyi@ensai.fr>
2017-12-07 10:35:25 +0000

    version 0.1.0

---
+++ src/TukeyRegion.h
@@ -0,0 +1,34 @@
+// TukeyRegion.h
+// By Pavlo Mozharovskyi
+// Last changed 06.12.2017
+// Header to C++-callable functions for the Tukey region and its elements
+
+#pragma once
+
+#include <Rcpp.h>
+using namespace Rcpp;
+
+#include <vector>
+#include <algorithm>
+#include <boost/dynamic_bitset.hpp>
+#include <queue>
+using namespace std;
+
+extern "C"{
+#include "libqhull.h"
+#include "qhull_a.h"
+#include "glpk.h"
+}
+
+#include "common.h"
+#include "hypermatrix.h"
+#include "bstree.h"
+#include "TRegion.h"
+#include "TConvexBody.h"
+
+#include <math.h>
+#include <cstdlib>
+#include <string>
+
+#include "qhAdapter.h"
+#include <boost/random.hpp>
